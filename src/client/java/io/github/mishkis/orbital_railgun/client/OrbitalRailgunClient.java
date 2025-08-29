c72758d1fb9ddbd51f2651557887b06cf596266f
Mishkis <misha.o.gm@gmail.com>
2025-08-29 13:04:00 -0300

    feat: added orbital railgun model

--- src/client/java/io/github/mishkis/orbital_railgun/client/OrbitalRailgunClient.java
+++ src/client/java/io/github/mishkis/orbital_railgun/client/OrbitalRailgunClient.java
@@ -1,10 +1,26 @@
 package io.github.mishkis.orbital_railgun.client;
 
+import io.github.mishkis.orbital_railgun.OrbitalRailgun;
+import io.github.mishkis.orbital_railgun.client.item.OrbitalRailgunRenderer;
+import io.github.mishkis.orbital_railgun.item.OrbitalRailgunItems;
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.render.item.BuiltinModelItemRenderer;
+import software.bernie.geckolib.animatable.client.RenderProvider;
 
 public class OrbitalRailgunClient implements ClientModInitializer {
-
     @Override
     public void onInitializeClient() {
+        OrbitalRailgunItems.ORBITAL_RAILGUN.renderProviderHolder.setValue(new RenderProvider() {
+            private OrbitalRailgunRenderer renderer;
+
+            @Override
+            public BuiltinModelItemRenderer getCustomRenderer() {
+                if (this.renderer == null) {
+                    this.renderer = new OrbitalRailgunRenderer();
+                }
+
+                return this.renderer;
+            }
+        });
     }
 }
--- src/main/java/io/github/mishkis/orbital_railgun/item/OrbitalRailgunItem.java
+++ src/main/java/io/github/mishkis/orbital_railgun/item/OrbitalRailgunItem.java
@@ -1,11 +1,43 @@
 package io.github.mishkis.orbital_railgun.item;
 
+import io.github.mishkis.orbital_railgun.OrbitalRailgun;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.util.Rarity;
+import org.apache.commons.lang3.mutable.MutableObject;
+import software.bernie.geckolib.animatable.GeoItem;
+import software.bernie.geckolib.animatable.client.RenderProvider;
+import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
+import software.bernie.geckolib.core.animation.AnimatableManager;
+import software.bernie.geckolib.util.GeckoLibUtil;
 
-public class OrbitalRailgunItem extends Item {
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class OrbitalRailgunItem extends Item implements GeoItem {
+    private final AnimatableInstanceCache CACHE = GeckoLibUtil.createInstanceCache(this);
+    private final Supplier<Object> renderProvider = GeoItem.makeRenderer(this);
+    public final MutableObject<RenderProvider> renderProviderHolder = new MutableObject<>();
+
     public OrbitalRailgunItem() {
         super(new FabricItemSettings().rarity(Rarity.EPIC).maxCount(1));
+    }
+
+    @Override
+    public void createRenderer(Consumer<Object> consumer) {
+        consumer.accept(this.renderProviderHolder.getValue());
+    }
+
+    @Override
+    public Supplier<Object> getRenderProvider() {
+        return renderProvider;
+    }
+
+    @Override
+    public void registerControllers(AnimatableManager.ControllerRegistrar controllerRegistrar) {}
+
+    @Override
+    public AnimatableInstanceCache getAnimatableInstanceCache() {
+        return CACHE;
     }
 }
